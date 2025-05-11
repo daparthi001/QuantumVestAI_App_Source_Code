8a9cf74a6eddb4c3dfb057d47996dff4cb9d1ee3
gayatre devi daparthi <daparthi.gayatri@gmail.com>
2025-05-11 08:42:12 -0500

    updated

--- ci-cd/terraform/main.tf
+++ ci-cd/terraform/main.tf
@@ -17,7 +17,7 @@ module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
-  subnets         = module.vpc.private_subnets
+  vpc_subnet_ids  = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
   eks_managed_node_groups = {
