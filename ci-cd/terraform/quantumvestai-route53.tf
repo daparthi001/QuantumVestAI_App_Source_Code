84fd30a377d19bddf006f9994f5e58c54651a734
gayatre devi daparthi <daparthi.gayatri@gmail.com>
2025-05-11 08:30:27 -0500

    updated

--- ci-cd/terraform/quantumvestai-route53.tf
+++ ci-cd/terraform/quantumvestai-route53.tf
@@ -1,8 +1,3 @@
-tags = {
-    Name = "quantumvestai-cert"
-  }
-}
-
 resource "aws_route53_record" "cert_validation" {
   name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
   type    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
