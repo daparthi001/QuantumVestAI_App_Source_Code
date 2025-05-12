3c460b964b34a76ee96c99c055fcb0a94b115be7
gayatre devi daparthi <daparthi.gayatri@gmail.com>
2025-05-12 15:27:55 -0500

    update

--- ci-cd/terraform/outputs.tf
+++ ci-cd/terraform/outputs.tf
@@ -1,8 +1,8 @@
-output "eks_cluster_endpoint" {
+output "cluster_endpoint" {
   value = module.eks.cluster_endpoint
 }
 
-output "eks_cluster_name" {
+output "cluster_name" {
   value = module.eks.cluster_name
 }
 
