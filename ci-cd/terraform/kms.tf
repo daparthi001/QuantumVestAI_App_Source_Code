3862f4a99827a57647c7547a0174d47da1711f0a
gayatre devi daparthi <daparthi.gayatri@gmail.com>
2025-05-12 11:39:47 -0500

    updated

--- ci-cd/terraform/kms.tf
+++ ci-cd/terraform/kms.tf
@@ -5,6 +5,6 @@ resource "aws_kms_key" "eks_key" {
 }
 
 resource "aws_kms_alias" "eks_key_alias" {
-  name          = "alias/eks/quantumvestai-eks"
+  name          = "alias/eks/quantumvestai-eks2"
   target_key_id = aws_kms_key.eks_key.key_id
 }
