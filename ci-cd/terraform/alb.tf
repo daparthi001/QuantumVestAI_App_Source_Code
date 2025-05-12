ef37ef7e41bcfeeb23645db6b01b960c2dc85b23
gayatre devi daparthi <daparthi.gayatri@gmail.com>
2025-05-12 10:36:42 -0500

    updated

--- ci-cd/terraform/alb.tf
+++ ci-cd/terraform/alb.tf
@@ -2,8 +2,7 @@ resource "aws_lb" "app" {
   name               = "dummy-alb-quantumvestai"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = []
-  subnets            = ["subnet-abc123"]  # Replace with real subnet IDs
+  subnets            = module.vpc.public_subnets
 
   enable_deletion_protection = false
 }
