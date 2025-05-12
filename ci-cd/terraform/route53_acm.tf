fae27f4cd1c96f2c9b85228aa88110c047046c77
gayatre devi daparthi <daparthi.gayatri@gmail.com>
2025-05-12 09:49:12 -0500

    updated

--- ci-cd/terraform/route53_acm.tf
+++ ci-cd/terraform/route53_acm.tf
@@ -23,7 +23,4 @@ resource "aws_route53_record" "validation" {
   ttl     = 60
   records = [each.value.value]
 }
-resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
-}
+
