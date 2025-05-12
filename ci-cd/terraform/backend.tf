7d0d5146e1f6ad47b0e32bf2468889194510debb
gayatre devi daparthi <daparthi.gayatri@gmail.com>
2025-05-12 17:18:00 -0500

    update

--- ci-cd/terraform/backend.tf
+++ ci-cd/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "quantumvestai-state-bucket"
-    key            = "quantumvestai/terraform.tfstate"
+    key            = "quantumvestai/eks/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "quantumvestai-lock-table"
     encrypt        = true
