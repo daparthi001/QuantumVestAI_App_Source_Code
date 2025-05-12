2b362a36c34d00694b83ea1c1c77ab6f231299b1
gayatre devi daparthi <daparthi.gayatri@gmail.com>
2025-05-12 00:03:33 -0500

    updated terraform

--- ci-cd/terraform/main.tf
+++ ci-cd/terraform/main.tf
@@ -15,16 +15,18 @@ module "vpc" {
 
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
+  version         = "20.8.4" # Best practice to pin latest stable (update if older)
+
   cluster_name    = var.cluster_name
   cluster_version = "1.29"
-  subnet_ids = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     default = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
+      desired_size = 2
+      max_size     = 3
+      min_size     = 1
 
       instance_types = ["t3.medium"]
     }
