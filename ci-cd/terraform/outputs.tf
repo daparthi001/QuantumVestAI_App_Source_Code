5966db85a4bc37e96bd1cd500049dd9f997f7867
hemanth <75059201+hemanth9398@users.noreply.github.com>
2025-05-13 13:30:47 -0500

    Update outputs.tf

--- ci-cd/terraform/outputs.tf
+++ ci-cd/terraform/outputs.tf
@@ -1,68 +1,136 @@
-# Consolidated Output Definitions
+# Consolidated Output Definitions for QuantumVestAI Infrastructure
 
-# General outputs
-# Remove duplicate outputs that are already in main.tf
+# AWS Account Outputs
+output "account_id" {
+  description = "AWS Account ID"
+  value       = data.aws_caller_identity.current.account_id
+}
+
+output "region" {
+  description = "AWS Region"
+  value       = var.region
+}
+
+# EKS Cluster Outputs
+output "cluster_name" {
+  description = "Name of the EKS cluster"
+  value       = aws_eks_cluster.eks.name
+}
+
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = aws_eks_cluster.eks.endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "Security group ID of the EKS cluster"
+  value       = aws_eks_cluster.eks.vpc_config[0].cluster_security_group_id
+}
+
+output "cluster_certificate_authority_data" {
+  description = "Base64 encoded certificate data required to communicate with the cluster"
+  value       = aws_eks_cluster.eks.certificate_authority[0].data
+  sensitive   = true
+}
 
 # VPC Outputs
 output "vpc_id" {
-  description = "VPC ID"
+  description = "ID of the VPC"
   value       = module.vpc.vpc_id
 }
 
-output "private_subnets" {
-  description = "Private subnet IDs"
+output "private_subnet_ids" {
+  description = "List of private subnet IDs"
   value       = module.vpc.private_subnets
 }
 
-output "public_subnets" {
-  description = "Public subnet IDs"
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
   value       = module.vpc.public_subnets
 }
 
-# EKS Outputs
-# Keep outputs from eks_cluster.tf that aren't duplicated elsewhere
-
 # Load Balancer Outputs
-# Remove duplicates with loadbalancer.tf
-# output "alb_dns_name" { ... } - Already in loadbalancer.tf
-# output "alb_zone_id" { ... } - Already in loadbalancer.tf
-# output "target_group_arn" { ... } - Already in loadbalancer.tf
+output "alb_dns_name" {
+  description = "DNS name of the load balancer"
+  value       = aws_lb.app.dns_name
+}
+
+output "alb_zone_id" {
+  description = "Zone ID of the load balancer"
+  value       = aws_lb.app.zone_id
+}
+
+output "target_group_arn" {
+  description = "ARN of the target group"
+  value       = aws_lb_target_group.app.arn
+}
 
 # RDS Outputs
 output "rds_endpoint" {
   description = "Endpoint of the RDS instance"
-  value       = aws_db_instance.main.endpoint
+  value       = aws_db_instance.quantumvestai.endpoint
+  sensitive   = true
 }
 
 output "rds_port" {
   description = "Port of the RDS instance"
-  value       = aws_db_instance.main.port
+  value       = aws_db_instance.quantumvestai.port
 }
 
 output "rds_database_name" {
-  description = "Name of the database"
-  value       = aws_db_instance.main.name
-}
-
-output "rds_secret_name" {
-  description = "Name of the Kubernetes secret containing RDS credentials"
-  value       = kubernetes_secret.rds_credentials.metadata[0].name
+  description = "Name of the RDS database"
+  value       = aws_db_instance.quantumvestai.db_name
 }
 
 # CloudFront Outputs
-# Remove duplicates with cloudfront.tf
-# output "cloudfront_distribution_id" { ... } - Already in cloudfront.tf
-# output "cloudfront_domain_name" { ... } - Already in cloudfront.tf
+output "cloudfront_distribution_id" {
+  description = "ID of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.api_cdn.id
+}
+
+output "cloudfront_domain_name" {
+  description = "Domain name of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.api_cdn.domain_name
+}
+
+output "cloudfront_hosted_zone_id" {
+  description = "Hosted zone ID of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.api_cdn.hosted_zone_id
+}
+
+# ECR Outputs
+output "ecr_repository_url" {
+  description = "URL of the ECR repository"
+  value       = aws_ecr_repository.quantumvestai.repository_url
+}
+
+# Secrets Manager Outputs
+output "rds_secret_name" {
+  description = "Name of the Secrets Manager secret for RDS credentials"
+  value       = aws_secretsmanager_secret.rds_credentials.name
+  sensitive   = true
+}
 
 # KMS Outputs
-# Remove duplicates with kms-encryption.tf
-# output "eks_kms_key_arn" { ... } - Already in kms-encryption.tf
-# output "rds_kms_key_arn" { ... } - Already in kms-encryption.tf
+output "eks_kms_key_arn" {
+  description = "ARN of the KMS key used for EKS encryption"
+  value       = aws_kms_key.eks_key.arn
+}
 
-# Route53 Outputs
+output "rds_kms_key_arn" {
+  description = "ARN of the KMS key used for RDS encryption"
+  value       = aws_kms_key.rds_key.arn
+}
+
+output "secrets_kms_key_arn" {
+  description = "ARN of the KMS key for Secrets Manager encryption"
+  value       = aws_kms_key.secrets_key.arn
+}
+
+# Route 53 Outputs
 output "route53_zone_id" {
-  description = "Route53 zone ID"
-  value       = var.route53_zone_id
+  description = "ID of the Route 53 hosted zone"
+  value       = var.route53_zone_id != "" ? var.route53_zone_id : aws_route53_zone.main[0].zone_id
 }
 
 output "acm_certificate_arn" {
