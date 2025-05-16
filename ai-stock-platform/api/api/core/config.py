a284e7f20ba50a3fa44909659f154e36ad4838d9
gayatre devi daparthi <daparthi.gayatri@gmail.com>
2025-05-16 14:45:45 -0500

    update

--- ai-stock-platform/api/api/core/config.py
+++ ai-stock-platform/api/api/core/config.py
@@ -2,7 +2,8 @@ import os
 import secrets
 from typing import Any, Dict, List, Optional, Union
 
-from pydantic import AnyHttpUrl, BaseSettings, PostgresDsn, validator
+from pydantic_settings import BaseSettings
+from pydantic import AnyHttpUrl, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
