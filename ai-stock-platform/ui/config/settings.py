dabb99f7e6c986f1422c81ed2a6ebff1e362a9d2
gayatre devi daparthi <daparthi.gayatri@gmail.com>
2025-06-14 18:19:52 -0500

    update

--- ai-stock-platform/ui/config/settings.py
+++ ai-stock-platform/ui/config/settings.py
@@ -1,5 +1,5 @@
 import os
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from pydantic import validator  # validator is still in pydantic
 from typing import Optional
 
